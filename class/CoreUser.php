cc2377909f6c6127798a5f09ddc2b6631e051f19
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-17 11:19:51 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/core/coreuser/trunk@17 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/CoreUser.php
+++ class/CoreUser.php
@@ -1,13 +1,19 @@
 <?php
 namespace oc\ext\coreuser ;
 
+use jc\mvc\model\db\orm\ModelAssociationMap;
+
 use oc\ext\Extension;
 
 class CoreUser extends Extension
 {
-	
 	public function load()
 	{
+		// 向系统的模型关系图中添加 数据表配置
+		$aModelAssocMap = ModelAssociationMap::singleton() ;
+		
+		// user 表
+		// $aModelAssocMap->addOrm() ;
 	}
 	
 }
