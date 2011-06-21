cc975e25aedf36a23a0cbba5c46bc9dcc95046ce
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-21 02:20:06 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/core/coreuser/trunk@18 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/CoreUser.php
+++ class/CoreUser.php
@@ -9,11 +9,17 @@ class CoreUser extends Extension
 {
 	public function load()
 	{
+		///////////////////////////////////////
 		// 向系统的模型关系图中添加 数据表配置
 		$aModelAssocMap = ModelAssociationMap::singleton() ;
 		
 		// user 表
 		// $aModelAssocMap->addOrm() ;
+		
+		
+		///////////////////////////////////////
+		// 向系统添加控制器
+		// $this->application()->accessRouter()->addController('xxxx', "full class name") ;
 	}
 	
 }
