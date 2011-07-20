c710f857e994868135ff01e21085fbe6df5808f3
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-20 11:11:18 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/core/coreuser/trunk@302 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/CoreUser.php
+++ class/CoreUser.php
@@ -12,9 +12,32 @@ class CoreUser extends Extension
 {
 	public function load()
 	{
-	    
-    	    // 取得模型关系图的单件实例
-            $aAssocMap = PrototypeAssociationMap::singleton() ;
+		// 定义ORM
+        $this->defineOrm(PrototypeAssociationMap::singleton()) ;
+            
+		
+		///////////////////////////////////////
+		// 向系统添加控制器
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Register",'register','') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Login",'login','') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Logout",'logout','') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Update",'update') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Index",'subscribe.index') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Create",'subscribe.create') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Remove",'subscribe.remove') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Switchuser",'switch') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\AllUser",'alluser') ;
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\MyFriend",'myfriend','') ;
+		
+		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\MessageAndQueuePrototype",'all_message_types') ;
+		
+		
+		$this->application()->accessRouter()->setDefaultController('myfriend') ;
+	}
+	
+	
+	public function defineOrm(PrototypeAssociationMap $aAssocMap)
+	{
     	    $aAssocMap->addOrm(
                 	array(
                 		'keys' => 'uid' ,
@@ -78,27 +101,7 @@ class CoreUser extends Extension
             		),
             	)
             );
-            
-		
-		///////////////////////////////////////
-		// 向系统添加控制器
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Register",'register','') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Login",'login','') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Logout",'logout','') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Update",'update') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Index",'subscribe.index') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Create",'subscribe.create') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\subscribe\\Remove",'subscribe.remove') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\Switchuser",'switch') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\AllUser",'alluser') ;
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\MyFriend",'myfriend','') ;
-		
-		$this->application()->accessRouter()->addController("oc\\ext\\coreuser\\MessageAndQueuePrototype",'all_message_types') ;
-		
-		
-		$this->application()->accessRouter()->setDefaultController('myfriend') ;
 	}
-	
 }
 
 ?>
